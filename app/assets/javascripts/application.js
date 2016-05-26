1992c53ed7c1b01405b356082ee5d0da34436f34
Matthew Jelling <Jelling@MacBook-Pro.local>
2016-05-26 15:33:19 -0400

    adds delete functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,23 @@
 var bulletinBoard = angular.module("Bulletin-Board", []);
 
 bulletinBoard.controller("BulletinsController", ["$scope", "$http", function($scope, $http){
-  $http.get('/api/bulletins').then(function(response){
-    //pull an array of bulletins
-    $scope.bulletins = response.data.bulletins;
-    console.log("success on json request!");
-  })
-}])
+  $scope.renderBulletins = function(){
+    $http.get('/api/bulletins').then(function(response){
+      //pull an array of bulletins
+      $scope.bulletins = response.data.bulletins;
+      console.log("success on json request!");
+    })
+  };
+  $scope.renderBulletins();
+
+  $scope.deleteBulletin = function(bulletinID){
+    $http.delete('/api/bulletins/'+ bulletinID).success(function(bulletinID){
+      console.log('bulletin trashed')
+    }).then(function(){
+      $scope.renderBulletins();
+    })
+  };
+
+  $scope.
+
+}]);
--- app/controllers/api/bulletins_controller.rb
+++ app/controllers/api/bulletins_controller.rb
@@ -5,6 +5,13 @@ class Api::BulletinsController < ApplicationController
   end
 
   def create
+    new_bulletin = Bulletin.create bulletin_params
+    render json: {bulletins: new_bulletin}
+  end
 
+  def destroy
+    trash_bulletin = Bulletin.find params[:id]
+    trash_bulletin.destroy
+    render json: {bulletins: Bulletin.all}
   end
 end
