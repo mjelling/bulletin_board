62cd9a63999f45b2e3c8c962d1ec0bd533046268
Matthew Jelling <Jelling@MacBook-Pro.fios-router.home>
2016-05-26 21:50:46 -0400

    pre-rework of post div

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,47 @@ bulletinBoard.controller("BulletinsController", ["$scope", "$http", function($sc
     })
   };
 
-  $scope.
+  $scope.launchNewBox = function(){
+    $('.bulletin').prepend($('<div>').addClass('box').attr('id', 'post_box').append($("<h3>").text("Input Your Name")).append($("<input>",
+         { type:'text',
+           name:'posted_by'
+         }
+     ).addClass('posted_by')).append($("<br>")).append($("<h3>").text("Post Content")).
+     append($("<input>",
+          { type:'text',
+            name:'posted_content',
+            width: '90%',
+            height: '30em'
+          }
+      ).addClass('post_content')).append($("<br>")).
+      append($('<button>').attr('ng-click', '"postBuulletin()"'))
+    );
+};
+        //$("<input>",
+          // { type:'submit',
+            // value:"Submit",
+             //ng-click: postBulletin()
+          //}
+
+
+
+  $scope.postBulletin = function(){
+    var $postedBy = $('.posted_by').value;
+    var $postContent = $('.posted_content').value;
+    var newBulletin = {
+      bulletin: {
+        posted_by: $postedBy,
+        post_content: $postContent
+      }
+    }
+    $http.post('/api/bulletins', newBulletin)
+   .success(function (newBulletin) {
+   console.log("bulletin posted")
+ }).then(function(){
+   $scope.bulletin = {};
+   $scope.renderBulletins();
+   console.log('post function');
+ })
+}
 
 }]);
